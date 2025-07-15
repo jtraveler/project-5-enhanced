19e9851fa94ba3fce1eef74807720108718c1583
jtraveler <matthew.jtraveler@gmail.com>
2025-07-15 07:00:10 +0200

    added home app and templates

--- my_special_shop/settings.py
+++ my_special_shop/settings.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 """
 Django settings for my_special_shop project.
 
@@ -9,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 
@@ -82,7 +84,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-          BASE_DIR / 'templates',
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
