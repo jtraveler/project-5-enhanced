ea8e0c5241f4e512968266408ba4981770aa5a62
jtraveler <matthew.jtraveler@gmail.com>
2025-07-26 10:26:30 +0200

    feat: Completeed Stripe payment integration - Add payment form submision handler Implemented stripe.confirmCardPayment method - Added eror handling and form states

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
-import os
 
 from .forms import OrderForm
 from bag.contexts import bag_contents
 
 import stripe
 
-# Get Stripe keys from environment variables
-stripe_public_key = os.getenv('STRIPE_PUBLIC_KEY', '')
-stripe_secret_key = os.getenv('STRIPE_SECRET_KEY', '')
-
 def checkout(request):
-    stripe.api_key = stripe_secret_key
-    
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -23,17 +19,23 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
     
     order_form = OrderForm()
-    
+
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
-    
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test_client_secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
