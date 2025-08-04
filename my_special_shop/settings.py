b5b614ba72b065ca1ee97db1efa6604e3bf21fdb
jtraveler <matthew.jtraveler@gmail.com>
2025-08-04 10:07:33 +0200

    fix: Corrected duplicate DEFAULT_FROM_EMAIL and update to nycdogpro@gmail.com

--- my_special_shop/settings.py
+++ my_special_shop/settings.py
@@ -199,8 +199,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
-
+    DEFAULT_FROM_EMAIL = 'nycdogpro@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -208,11 +207,10 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-DEFAULT_FROM_EMAIL = 'botiqueado@example.com'
