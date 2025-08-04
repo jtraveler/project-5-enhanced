0c555461f0ef4c4cfb4f22c2ffef5fb424f17770
jtraveler <matthew.jtraveler@gmail.com>
2025-08-04 16:31:40 +0200

    fix: Fallback defaults for S3 storages and bytes error in DATABASE_URL

--- custom_storages.py
+++ custom_storages.py
@@ -2,7 +2,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = getattr(settings, 'STATICFILES_LOCATION', 'static')
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = getattr(settings, 'MEDIAFILES_LOCATION', 'media')
