438569115e3de3c4807e8b33f7bb0d039d2c2623
jtraveler <matthew.jtraveler@gmail.com>
2025-08-05 01:55:04 +0200

    fix: Use console email backend in Heroku for signup

--- my_special_shop/settings.py
+++ my_special_shop/settings.py
@@ -139,17 +139,10 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 
 # Email
-if DEBUG:
+if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'nycdogpro@gmail.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'nycdogpro@gmail.com'
+
 
 # Logging
 LOGGING = {
