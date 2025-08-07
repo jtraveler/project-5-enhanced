1a120fb8d0ea8310dbafdf04d980e8645e1a477e
jtraveler <matthew.jtraveler@gmail.com>
2025-08-07 10:51:11 +0200

    feat: Added custom 404 and 500 error pages

--- my_special_shop/views.py
+++ my_special_shop/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to My Special Shop!")
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, 'errors/404.html', status=404)
+
+def handler500(request):
+    """ Error Handler 500 - Internal Server Error """
+    return render(request, 'errors/500.html', status=500)
